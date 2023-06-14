f9dcef08577d39ad05837b40eeb7ecbeca6dc352
Nurzhan <bakytov9798@gmail.com>
2023-06-14 17:54:41 +0600

    style fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRoute } from 'app/providers/routes';
-import './style/index.scss';
 import { Sidebar } from 'shared/ui/Sidebar';
 import { PageLoader } from 'shared/ui/PageLoader';
 import { Navbar } from 'widget/Navbar';
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Counter } from 'entity/Counter';
-import { Input } from 'shared/ui/Input';
 
 function MainPage() {
     const { t } = useTranslation();
